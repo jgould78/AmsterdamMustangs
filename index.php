7edeb6a213b1ffa25c27c911d96e8719a4d0c84a
jgould78 <jeremiegould@gmail.com>
2024-10-17 14:20:56 +0200

    PHP testing

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Amsterdam Mustangs</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php
+include('header.php');
+?>
+
+
+Text here
+
+<?php
+include('footer.php');
+?>
+ </body>
+</html>
